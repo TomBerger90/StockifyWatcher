c9d226f131c1276fa9bf031c18c3c652b95eea8a
Tom Arnaud <11574512+TomBerger90@users.noreply.github.com>
2023-11-29 00:45:42 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+/* DB connection parameters */
+$host = "localhost";
+$dbname = "";
+$username = "";
+$password = "";
+
+/* Twilio parameters */
+// Your Account SID from www.twilio.com/console
+$sid = '';
+//Your personal phone 
+$to = "whatsapp:+";
+// Your Twilio number
+$from = "whatsapp:+";
+// Token from www.twilio.com/console
+$token = "";
